3ec7c716aa2f62d4ebf153cb3fbd0837531b457f
Mercy Maruga <mercy.maruga@student.moringaschool.com>
2022-11-25 14:40:12 +0300

    completed assignment

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
-
+  
+  # GET /birds
   def show
     post = Post.find(params[:id])
     
@@ -9,9 +10,10 @@ class PostsController < ApplicationController
   def update
     post = Post.find(params[:id])
 
-    post.update(post_params)
-
+    post.update!(post_params)
     render json: post
+    rescue ActiveRecord::RecordInvalid => invalid
+    render json: { errors: invalid.record.errors }, status: :unprocessable_entity
   end
 
   private
@@ -19,5 +21,6 @@ class PostsController < ApplicationController
   def post_params
     params.permit(:category, :content, :title)
   end
+  
 
 end
